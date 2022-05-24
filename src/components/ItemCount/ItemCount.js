0c25b44d5a119034de8d224ba3a52071cabab8bc
lockus96 <angelmcarrizo@gmail.com>
2022-05-24 16:17:28 -0300

    style

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -19,10 +19,12 @@ const ItemCount = ({stock, initial, onAdd}) =>{
      return (
           <div className="card contador">
                <div className="card-body">
-                    <button className="btn btn-primary" onClick={disminuir}> - </button>
-                    <button className="btn btn-primary" onClick={aumentar}> + </button>
+                    <div className="row">
+                    <button className="btn btn-outline-dark btn-sm col-6" onClick={disminuir}> - </button>
+                    <button className="btn btn-outline-dark btn-sm col-6" onClick={aumentar}> + </button>
+                    </div>
                     <h5 className="card-title numeroContador">{cuenta}</h5>
-                    <button className="btn btn-primary" onClick={agregarTotal}> Agregar </button>
+                    <button className="btn btn-success" onClick={agregarTotal}> Agregar </button>
                </div>
           </div> 
 
