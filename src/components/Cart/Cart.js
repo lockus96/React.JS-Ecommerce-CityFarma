f9c21940f8d44ec29ddbd216ccd4afb46546aa24
lockus96 <angelmcarrizo@gmail.com>
2022-07-02 23:37:03 -0300

    style final

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,25 +12,25 @@ const Cart = () => {
 
      return (
           cart.length === 0 ? <div className="carritoVacio"><h2 className="carritoVacioTitle">Ups! No hay nada en el carrito</h2></div> :    
-          <div className="cartStyle">
-               <h2 className="carritoText"> Carrito </h2>
+          <div className="cartBody">
                <div className="btnClear">
-                    <div className="btn btn-danger" onClick={() => {clear()}}>Limpiar carrito </div>
+                    <div className="btn btn-danger borrarCarrito" onClick={() => {clear()}}>Limpiar carrito </div>
                </div>
-               <div>
+               <div className="grillaCart">
                     {cart.map(item => (
-                         <div key={item.id} className='cartItems'>
-                              <div className="imgContainer">
-                                    <img src={item.img} alt={item.name} className='imgCart' />
-                              </div>
-                              <div className="textItemCart">
+
+                    <div key={item.id} className='cartList' >
+                         <div className="cardList">
+                              <img src={item.img} alt={item.name} className='imgList' />
+                              <div>
                                    <p>{item.nombre}</p>
                                    <p>{item.precio}</p>
                                    <p>Cantidad:{item.cantidad}</p>
-                                   <div className="btn btn-danger" onClick={() => { removeItem(item.id)}}>Eliminar</div>
+                                   <div className="btn btn-danger" onClick={() => { removeItem(item.id)}}>Eliminar
                               </div>
-                             
                          </div>
+                    </div>
+               </div>
                     ))}         
                </div>       
           </div>
