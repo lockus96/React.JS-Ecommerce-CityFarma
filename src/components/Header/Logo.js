c0aef747801afdfc1d776c22c384c5ac08e04f5f
Miguel Angel Carrizo <angelmcarrizo@gmail.com>
2022-09-19 23:36:09 -0300

    fix logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
      return (
                <div className="logo-position">
                     <Link to='/'>
-                    <img src="https://i.imgur.com/8Lhgo08.png" alt="logo" style={{ width: 200, height: 200 }} />
+                    <img src="https://i.imgur.com/8Lhgo08.png" alt="logo" style={{ width: 200, height: 170 }} />
                     </Link>
                </div>
           )
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -12,16 +12,7 @@ import {
    } from '@chakra-ui/react';
 import { FiShoppingCart } from 'react-icons/fi';
 
-const data = {
-     isNew: true,
-     imageURL:
-       'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
-     name: 'Wayfarer Classic',
-     price: 4.5,
-     rating: 4.2,
-     numReviews: 34,
-};
-   
+
 
 
 
@@ -44,7 +35,7 @@ const ItemList = (props) =>{
                     rounded="lg"
                     shadow="lg"
                     position="relative">
-                    {data.isNew && (
+                    {item.estado && (
                          <Circle
                          size="10px"
                          position="absolute"
@@ -65,7 +56,7 @@ const ItemList = (props) =>{
 
                     <Box p="6">
                          <Box d="flex" alignItems="baseline">
-                         {data.isNew && (
+                         {item.estado && (
                          <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
                               Nuevo
                          </Badge>
