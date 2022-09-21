073b478cb5eeddf95f87e5f141371bed7e5c5f20
lockus96 <angelmcarrizo@gmail.com>
2022-09-21 12:42:25 -0300

    logo animation

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom'
+import { motion } from "framer-motion";
 
 
 const Logo = () => {
 
 
      return (
-               <div className="logo-position">
-                    <Link to='/'>
-                    <img src="https://i.imgur.com/8Lhgo08.png" alt="logo" style={{ width: 500, height: 170 }} />
-                    </Link>
-               </div>
+          <motion.div
+          className="box"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}>
+                    <div className="logo-position" style={{ width: 250 }}>
+                         <Link to='/'>
+                         <img src="https://i.imgur.com/8Lhgo08.png" alt="logo"  />
+                         </Link>
+                    </div>
+          </motion.div>
           )
      }
 export default Logo
