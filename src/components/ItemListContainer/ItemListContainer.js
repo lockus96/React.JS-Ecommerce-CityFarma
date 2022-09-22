300dc2b83c91982e39a65b46900781c698efe3c4
Miguel Angel Carrizo <angelmcarrizo@gmail.com>
2022-09-22 15:52:56 -0300

    useLocation and slice home

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,19 +2,20 @@ import {useState, useEffect} from 'react'
 import ItemList from '../ItemList/ItemList';
 import { getDocs, collection, where, query } from 'firebase/firestore'
 import { db } from '../../services/firebase'
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 
 
 
 const ItemListContainer = () =>{
 
-     const [productos, setProductos] = useState([]);
-
      
+     const [home, setHome] = useState()
+     const [productos, setProductos] = useState([]);
      const {categoryId} = useParams()
-     console.log(productos)
-  
+     const location = useLocation();
+
+   
 
      useEffect(() => {
 
@@ -22,21 +23,36 @@ const ItemListContainer = () =>{
           ? query (collection(db, 'products'), where('categoria', '==', categoryId))
           : collection(db, 'products')
 
-          getDocs(collectionRef).then(data => {
-               const products = data.docs.map(doc => {
-                    return { id: doc.id, ...doc.data()}
-                    
+          // eslint-disable-next-line
+          if(location.pathname == '/' ||location.pathname == '' ){
+               setHome(true)
+               getDocs(collectionRef).then(data => {
+                    const products = data.docs.slice(0, 3).map(doc => {
+                         return { id: doc.id, ...doc.data()}
+                         
+                    })
+                    setProductos(products)
+               }).catch(err => {
+                    console.log(err)
                })
-               setProductos(products)
-          }).catch(err => {
-               console.log(err)
-          })
-     }, [categoryId])
-     
-    
+          } else {
+               setHome(false)
+               getDocs(collectionRef).then(data => {
+                    const products = data.docs.map(doc => {
+                         return { id: doc.id, ...doc.data()}
+                         
+                    })
+                    setProductos(products)
+               }).catch(err => {
+                    console.log(err)
+               })
+          }
+   
 
+     }, [categoryId, location.pathname])
+     
      return (
-          <div className='grillaItems'>
+          <div className={ home ? 'grillaHome' : 'grillaItems'  } >
           <ItemList producto={productos}/>
           </div>
      )
